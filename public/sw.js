4928167f319a5b4ffbf3d6d92c0d9373208847f9
Ralica Popova <ralicaterzieva90@gmail.com>
2024-12-04 16:38:46 +0200

    Add fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function(event) {
     console.log('Activating Service worker...', event)
     return self.clients.claim(); //-> ensures the service workers are activated correctly
 })
+
+self.addEventListener('fetch', function(event) {
+    console.log('SW Fetching Something...', event)
+    event.respondWith(fetch(event.request)) // Override the data that was sent back
+})
