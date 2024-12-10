85a859b41ec47624f31fd3b180b470f3912d5cf3
Ralitsa Popova <ralicaterzieva90@gmail.com>
2024-12-10 15:31:59 +0000

    Refactor strategy logic

--- public/sw.js
+++ public/sw.js
@@ -77,28 +77,15 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
       fetch(event.request)
-        .cstch(function(err) {
-            caches.match(event.request)
-            .then(function(response) {
-              if (response) {
-                return response;
-              } else {
-                return fetch(event.request)
-                    .then(function (res) {
-                        return caches.open(CACHE_DYNAMIC_NAME)
-                            .then(function(cache) {
-                                cache.put(event.request.url, res.clone())
-                                return res
-                            })
+        .then(function(response) {
+            return caches.open(CACHE_DYNAMIC_NAME)
+                    .then(function(cache) {
+                        cache.put(event.request.url, response.clone())
+                            return res
                     })
-                    .catch(function(error) {
-                        return caches.open(CACHE_STATIC_NAME)
-                            .then(function(cache) {
-                                return cache.match('/offline.html')
-                            })
-                    })
-              }
-            })
+        })
+        .catch(function(err) {
+           return caches.match(event.request) 
         })
     );
   });
