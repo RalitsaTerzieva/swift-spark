424e6c2d092cea078dced45f22961146b50a2d69
Ralica Popova <ralicaterzieva90@gmail.com>
2024-12-14 16:30:52 +0200

    Add cache only strategy

--- public/sw.js
+++ public/sw.js
@@ -24,22 +24,7 @@ this.addEventListener('install', function(event) {
         caches.open(CACHE_STATIC_NAME)
             .then(function(cache) {
                 console.log('Precaching App Shell')
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/offline.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/promise.js',
-                    '/src/js/fetch.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/main-image.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                  ])
+                cache.addAll(STATIC_FILES)
             })
             .catch(function(error) {
                 console.error('Precaching failed:', error);
@@ -76,7 +61,10 @@ self.addEventListener('fetch', function(event) {
                       })
               })
           )
-    } else if ( ) {
+    //check if my url is part of this array files
+    } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+        //cache only startegy for static files
+        event.respondWith(caches.match(event.request))
     } else {
         event.respondWith(caches.match(event.request)
         .then(function(response) {
