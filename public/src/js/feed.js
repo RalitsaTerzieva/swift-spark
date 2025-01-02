09cdecd18c89e4469cdbf6ae7c1cd874574b14dd
Ralica Popova <ralicaterzieva90@gmail.com>
2025-01-02 17:09:44 +0400

    Register synchronization task

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -152,6 +152,13 @@ form.addEventListener('submit', function(event) {
     }
 
     closeCreatePostModal();
+
+    if('serviceWorker' in navigator && 'SyncManager' in window) {
+        navigator.serviceWorker.ready
+            .then(function(sw) {
+                sw.sync.register('sync-new-post');
+            })
+    }
 })
 
 
