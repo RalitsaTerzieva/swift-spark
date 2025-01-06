0d19ac90e4dbcceec1ccbdd80b32c12c29938df9
Ralica Popova <ralicaterzieva90@gmail.com>
2025-01-06 17:25:27 +0400

    Add more advanced notification options

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -23,7 +23,9 @@ function displayConfirmNotification() {
             dir: 'ltr',
             lang: 'en-US',
             vibrate: [100, 50, 200],
-            badge: '/src/images/icons/app-icon-96x96.png'
+            badge: '/src/images/icons/app-icon-96x96.png',
+            tag: 'confirm-notification',
+            renotify: true
         };
 
         navigator.serviceWorker.ready
